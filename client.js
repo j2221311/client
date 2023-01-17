26d27f851abde74d2d8bd629d8cc86c82f1581b5
j2221311 <122776833+j2221311@users.noreply.github.com>
2023-01-17 17:18:26 +0900

    Update client.js

--- client.js
+++ client.js
@@ -3,6 +3,10 @@ window.__SKYWAY_KEY__ = '73c9b442-df7d-448f-a283-5d117794ba73';
 let localStream;
 const myVideo = document.getElementById('my-video');
 const theirVideo = document.getElementById('their-video');
+const toggleCamera = document.getElementById('js-toggle-camera');
+const toggleMicrophone = document.getElementById('js-toggle-microphone');
+const cameraStatus = document.getElementById('camera-status');
+const microphoneStatus = document.getElementById('microphone-status');
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
@@ -38,4 +42,16 @@ const setEventListener = mediaConnection => {
 peer.on('call', mediaConnection => {
     mediaConnection.answer(localStream);
     setEventListener(mediaConnection);
+});
+
+toggleCamera.addEventListener('click', () => {
+  const videoTracks = localStream.getVideoTracks()[0];
+  videoTracks.enabled = !videoTracks.enabled;
+  cameraStatus.textContent = `カメラ${videoTracks.enabled ? 'ON' : 'OFF'}`;
+});
+
+toggleMicrophone.addEventListener('click', () => {
+  const audioTracks = localStream.getAudioTracks()[0];
+  audioTracks.enabled = !audioTracks.enabled;
+  microphoneStatus.textContent = `マイク${audioTracks.enabled ? 'ON' : 'OFF'}`;
 });
